4e1b156cefab42c79f722335c85e2c9188ea89fd
WroblewskiAdam <adam01wroblewski@gmail.com>
2023-05-20 18:22:56 +0200

    ES

--- uma_projekt.py
+++ uma_projekt.py
@@ -1,33 +1,36 @@
 import numpy as np
+from cec2017.functions import f4 as func
 
-
-def init_population(dimension):
+def init_population(dimension, samples):
     # Inicjalizacja osobnika do ES 1+1
     # np.random.seed(10)
-    x = np.random.uniform(-100, 100, size=dimension)
+    x = np.random.uniform(-50, 50, size=(samples, dimension))
     return x
 
 
-def evaluate(x, function):
-    # Ocena osobnika w ES 1+1
-    pass
+def evaluate(x):
+    val = func(x)
+    return val
 
 
 def get_mutant(x, sigma, dimension):
     y = x + sigma * np.random.normal(loc=0.0, scale=1.0, size=dimension)
     return y
 
-def es(dim, k, init_sigma, max_epoch, function):
-    x = init_population(dim)
+
+def es_standard(dim, k, init_sigma, max_epoch):
+    x = init_population(dim,samples=1)
     succes_mem = []
-    sigma = init_sigma   
+    sigma = init_sigma
+    score_x = None
+    score_y = None
 
     epochs = 0
     while(epochs < max_epoch):
         y = get_mutant(x, sigma, dim)
         
-        score_x = evaluate(x, function)
-        score_y = evaluate(y, function)
+        score_x = evaluate(x)
+        score_y = evaluate(y)
         if score_y < score_x:
             succes_mem.append(True)
             x = y
@@ -39,17 +42,57 @@ def es(dim, k, init_sigma, max_epoch, function):
                 sigma = 1.22*sigma
             elif sum(succes_mem)/len(succes_mem) < 0.2:
                 sigma = 0.82*sigma
+            succes_mem = []
+        print((epochs, score_x[0]))
         epochs += 1
 
 
+    return (x, score_x)
+
+
+def es_rl(dim, k, init_sigma, max_epoch):
+    x = init_population(dim,samples=1)
+    succes_mem = []
+    sigma = init_sigma
+    score_x = None
+    score_y = None
+    succes_percent = 0
+
+    epochs = 0
+    while(epochs < max_epoch):
+        y = get_mutant(x, sigma, dim)
+        
+        score_x = evaluate(x)
+        score_y = evaluate(y)
+        if score_y < score_x:
+            succes_mem.append(True)
+            x = y
+        else:
+            succes_mem.append(False)
+
+        action = None # z RL
+
+        if epochs%k == 0:
+            if action == 1:
+                sigma = 1.22*sigma
+            elif action == 0:
+                sigma = sigma
+            elif action ==-1:
+                sigma = 0.82*sigma
+            succes_mem = []
+            succes_percent = sum(succes_mem/len(succes_mem))
+        print((epochs, score_x[0]))
+        epochs += 1
+
+
+    return (x, score_x)
+
 
 def main():
-    # dimension = 50
-    # x = init_population(dimension)
-    # y = get_mutant(x, dimension)
 
-    a = [True, False, True, True]
-    print(sum)
+    print(func)
+    a = es_standard(dim=2, k=10, init_sigma=1, max_epoch=10000)
+    print(a)
 
 
 
