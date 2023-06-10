faa53ddcee21a66cc5fbcc7389e9bb2344b2040a
Bargal1 <96236465+Bargal1@users.noreply.github.com>
2023-06-10 16:24:43 +0200

    Update create_models.py

--- create_models.py
+++ create_models.py
@@ -1,4 +1,4 @@
-from evolution_strategy import ES
+from evolution_strategy import ES, Reward
 from q_learning import Q_learn_agent
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,21 +7,21 @@ import numpy as np
 
 
 def main():
-    testing_k = [5, 10, 20, 50, 100, 200]
+    testing_k = [5,10, 20, 50,100]
     testing_beta = [0.1, 0.3, 0.5, 0.7, 0.9]
     testing_gamma = [0.1, 0.3, 0.5, 0.7, 0.9]
-    testing_epsilon = [0.3]
+    testing_epsilon = [0.1, 0.3, 0.5, 0.7, 0.9]
 
-    function = functions.f7
+    function = functions.f6
     dimension = 10 # stała
 
     k = 20
     iterations = 1000 # stała
     training_epochs = 200 # stała 
 
-    beta = 0.9
-    gamma = 0.9
-    epsilon = 0.3
+    beta = 0.5
+    gamma = 0.5
+    epsilon = 0.4
 
     data = {
         "srednia" : [],
@@ -31,11 +31,11 @@ def main():
     }
     
     for epsilon in testing_epsilon:
-        shape = [k+1, 16, 3]
+        shape = [11, 16, 3]
         Q_ag = Q_learn_agent(shape=shape, is_training=True, beta=beta, gamma=gamma, epsilon=epsilon)
         my_ES = ES(dimension=dimension, k=k, function=function)
-        my_ES.es_rl_training(max_epochs=training_epochs, max_iter=iterations, Q_ag=Q_ag)
-        file_name = f"model_g{int(gamma*10)}_b{int(beta*10)}_e{epsilon*10}_k{k}_f5.pickle" 
+        my_ES.es_rl_training(max_epochs=training_epochs, max_iter=iterations, Q_ag=Q_ag, reward=Reward.LogDiff)
+        file_name = f"model_g{int(gamma*10)}_b{int(beta*10)}_e{epsilon*10}_k{k}_f6alt.pickle" 
         Q_ag.dump_qfunction(file_name)#zapis do pliku
 
         result_list = []
@@ -43,8 +43,7 @@ def main():
         for i in range(30):
             Q_ag.is_training = False
             my_ES = ES(dimension=dimension, k=k, function=function)
-            result = my_ES.es_rl(max_iter=iterations, Q=Q_ag)
-            # result=my_ES.es_standard(max_iter=iterations)
+            result = my_ES.es_rl(iterations, Q_ag,reward=Reward.Percent)
             result_list.append(result)
         
         data["srednia"].append(np.mean(result_list))
@@ -57,8 +56,7 @@ def main():
     latex_code = latex_code.replace("\\\n", "\\ \hline\n")
     latex_code = latex_code.replace(".", ",")
     print(latex_code)
-    df.to_excel('f6_epsilon_testing.xlsx')
-
+    # df.to_excel('epsilon_testin1.xlsx')5
 
 
 
