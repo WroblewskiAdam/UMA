c8887bca56256cf016af7f02da0c55ba02bacb3e
Bargal1 <96236465+Bargal1@users.noreply.github.com>
2023-06-10 16:25:13 +0200

    Update evolution_strategy.py

--- evolution_strategy.py
+++ evolution_strategy.py
@@ -3,6 +3,13 @@ import pandas as pd
 from cec2017 import functions
 from q_learning import Q_learn_agent
 from matplotlib import pyplot as plt
+from enum import Enum
+
+class Reward(Enum):
+    Percent = 0
+    LogDiff = 1
+
+
 
 
 class ES:
@@ -61,20 +68,21 @@ class ES:
         return self.score_x
 
 
-    def es_rl_training(self, max_epochs, max_iter, Q_ag: Q_learn_agent):
+    def es_rl_training(self, max_epochs, max_iter, Q_ag: Q_learn_agent, reward = Reward.Percent):
         # distance_array = list()
         for _ in range(max_epochs):
             self.iteration = 0
             self.sigma = 1
             self.x = self.init_population()
             print("Epoka: ", _)
-            array = self.es_rl(max_iter, Q_ag)
+            array = self.es_rl(max_iter, Q_ag, reward=reward)
             self.training_epoch += 1
             # distance_array.extend(array)
 
 
-    def es_rl(self, max_iter, Q: Q_learn_agent):
+    def es_rl(self, max_iter, Q: Q_learn_agent, reward=Reward.Percent):
         self.init_population()
+        success_percent = 0
         distance_array = []
         state = ()
         action = 0
@@ -91,15 +99,17 @@ class ES:
                 self.success_mem.append(False)
 
             if self.iteration % self.k == 0:
-                reward2 = max(self.past_results) - min(self.past_results)
-                success_percent = round(sum(self.success_mem)/len(self.success_mem)*self.k)
+                reward = sum(self.success_mem)/len(self.success_mem)
+                reward2 = np.log2(max(self.past_results)) - np.log2(min(self.past_results))
+                success_percent = round(sum(self.success_mem)/len(self.success_mem)*10)
                 avg_distance = self.calc_distance()
                 distance_array.append(avg_distance)
                 action = Q.pick_action(state) if self.iteration!=0 else 0
+                # print(action)
                 match action:
                     case 1:
                         self.sigma = 0.82*self.sigma
-                    case 2:
+                    case 0:
                         self.sigma = 1.22*self.sigma
                     case _:
                         pass
@@ -110,7 +120,8 @@ class ES:
                 self.success_mem = []
                 self.past_results = []
                 if self.iteration!=0:
-                    Q.learn(reward2, prev_state, state, action)
+                    Q.learn(reward2, prev_state, state, action) if reward == Reward.LogDiff\
+                    else Q.learn(reward, prev_state, state, action)
 
             self.iteration += 1
             # print("Iter: %d, fcel: %d"%(self.iteration, self.evaluate(self.x)))
@@ -133,4 +144,3 @@ class ES:
     # def hist(self, array):
     #     plt.hist(array, bins='auto')
     #     plt.show()
-
